f4966073f3b4c65d0984597e60b5a8c9b1edb0ae
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-15 17:16:44 -0500

    update helpful message at top of s3man cfg

--- scripts/s3man.py
+++ scripts/s3man.py
@@ -5,10 +5,13 @@ import logging
 from dr_util.api_wrappers.aws_utils import S3Manager
 import dr_util.logging as lu
 
-# To test downloading from s3:
-#  uv run s3man 
-#      source.key=wikipedia_model/config.json
-#      output.file=refined/wikipedia_model_config.json
+# To test downloading from s3: uv run s3man
+# 
+# Cfg fields you most likely want to modify
+#  s3man_source.bucket  = refined.public
+#  s3man_source.key     = 2022_oct/wikipedia_model/config.json
+#  s3man_source.out_dir_name = refined
+#  output.file_name     = wiki_conf.json
 
 @hydra.main(version_base=None, config_path="../configs", config_name="s3man")
 def main(cfg: DictConfig) -> None:
