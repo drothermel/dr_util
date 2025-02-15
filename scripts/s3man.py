53d0e9792e9edc820aab55476e8ac1af962d68cc
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-15 16:28:42 -0500

    update the usage in the script

--- scripts/s3man.py
+++ scripts/s3man.py
@@ -4,23 +4,21 @@ import hydra
 from dr_util.api_wrappers.aws_utils import S3Manager
 from omegaconf import DictConfig
 
+# To test downloading from s3:
+#  uv run s3man 
+#      source.key=wikipedia_model/config.json
+#      output.file=refined/wikipedia_model_config.json
 
 @hydra.main(version_base=None, config_path="../configs", config_name="s3man_config")
 def main(cfg: DictConfig) -> None:
     logging.basicConfig(level="DEBUG")
 
-    # Recombine cfg into S3Manager inputs
-    source = cfg.s3man_source
-    bucket = source.bucket
-    key = f"{source.key_path_base}{source.key_name}"
-    outpath = f"{cfg.output.path_base}{cfg.output.file}"
-
     logging.info(">> Creating S3Manager and requesting file")
     s3m = S3Manager()
     s3m.download_file_if_needed(
-        s3_bucket=bucket,
-        s3_key=key,
-        output_file_path_str=outpath,
+        s3_bucket=cfg.s3man_source.bucket,
+        s3_key=cfg.s3man_source.key,
+        output_file_path_str=cfg.output.file,
     )
 
 
