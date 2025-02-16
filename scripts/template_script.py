7c227bce196ff615a4b1c3784624e1ad280f2785
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-02-16 14:35:50 -0500

    clean up the metrics example in template_script.py

--- scripts/template_script.py
+++ scripts/template_script.py
@@ -11,20 +11,17 @@ from dr_util.metrics import Metrics
 def run(cfg: DictConfig):
     logging.info(">> Welcome to your new script")
 
+    # Make Metrics and Log Cfg
     md = Metrics(cfg)
     md.log_cfg()
 
-    # Test Metrics Class
+    # Use Metrics Class
     for ns in random.choices(range(1, 11), k=100):
         loss = random.random()
         md.train({"loss": loss, "num_samples": ns}, ns)
         md.val({"loss": loss - 0.5, "num_samples": ns}, ns)
-
     md.agg_print("train")
-    print()
     md.agg_print("val")
-    print()
-
     logging.info(":: Goodbye ::")
 
 
