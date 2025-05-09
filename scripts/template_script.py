c0e37bd36891fa50f7562d6dbed81c005a978ada
Danielle Rothermel <danielle.rothermel@gmail.com>
2025-05-09 09:20:25 -0400

    update template script to demonstrate how to use data and det utils

--- scripts/template_script.py
+++ scripts/template_script.py
@@ -6,6 +6,8 @@ from omegaconf import DictConfig
 from dr_util.config_verification import validate_cfg
 from dr_util.metrics import Metrics
 from dr_util.schemas import get_schema
+import dr_util.data_utils as du
+import dr_util.determinism_utils as dtu
 
 
 @hydra.main(version_base=None, config_path="../configs", config_name="base_config")
@@ -16,11 +18,34 @@ def run(cfg: DictConfig):
     # Make Metrics and Log Cfg
     md = Metrics(cfg)
     md.log(cfg)
-    md.log(">> Welcome to your new script")
+    md.log(">> Welcome to your new script! Loading CIFAR-10.")
 
-    # Use Metrics Class
-    for ns in random.choices(range(1, 11), k=100):
-        loss = random.random()
+    # Setup dataset (split_seed) and dataloader (seed)
+    dataset = du.get_cifar_dataset(
+        cfg.data.name,
+        source_split=cfg.data.train.source_split,
+        transform=None,
+        root=cfg.paths.dataset_cache_root,
+        download=cfg.data.download,
+    )
+    train_dataset, val_dataset = du.split_data(
+        dataset, cfg.data.train.ratio, data_split_seed=cfg.data.split_seed,
+    )
+    generator = dtu.set_deterministic(cfg.seed)
+    train_dl = du.get_dataloader(
+        train_dataset,
+        du.get_tensor_transform(),
+        cfg.data.train.batch_size,
+        cfg.data.train.shuffle,
+        generator,
+        cfg.data.num_workers,
+    )
+
+    for idx, (data, labels) in enumerate(train_dl):
+        if idx > 2:
+            break
+        ns = len(data)
+        loss = sum(labels).item()
         md.train({"loss": loss, "num_samples": ns}, ns)
         md.val({"loss": loss - 0.5, "num_samples": ns}, ns)
     md.agg_log("train")
